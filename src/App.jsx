4f09f63c590f6d1ae1b1fee232bef79c98edb9b7
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-07 09:05:47 -0700

    Layout added to router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 import { Route } from "react-router-dom"
 import { Routes } from "react-router-dom"
 import { BrowserRouter } from "react-router-dom"
+import Layout from "./components/layout/Layout.jsx"
 
 /*
  * App component (Router)
@@ -14,7 +15,10 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="*" element={<>Hello World!</>} />
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<>Index</>} />
+                </Route>
+                <Route path="*" element={<>Page not found</>} />
             </Routes>
         </BrowserRouter>
     )
