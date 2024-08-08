343da2e1fea52bda145c725f63a0f59deb6a8b6c
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-08 15:24:38 -0700

    Friends page

---
+++ src/components/friends/Friends.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import styles from "./Friends.module.css"
+import AddIcon from "../../assets/add.svg"
+import Searchbar from "../shared/Searchbar.jsx"
+import ProfilePic from "../../assets/sample.png"
+import DeleteIcon from "../../assets/delete.svg"
+import { useEffect } from "react"
+
+/*
+ * Friends page
+ *
+ * @component
+ * @returns {JSX.Element} The rendered Friends page
+ */
+
+function Friends() {
+
+    const dumbFriendsData = [
+        {
+            id: 1,
+            code: "KAIL9875",
+            picture: ProfilePic,
+            begin_on: "05/02/2024",
+            username: "Luis Enrique Pacheco Torres",
+        },
+        {
+            id: 2,
+            code: "K9875IQO",
+            picture: ProfilePic,
+            begin_on: "04/02/2024",
+            username: "John Smith",
+        },
+    ]
+
+    const [query, setQuery] = useState("")
+    const [friends, setFriends] = useState([])
+    const [friendsCounter, setFriendsCounter] = useState(0)
+    const [filteredFriends, setFilteredFriends] = useState([])
+
+    useEffect(() => {
+        setFriends(dumbFriendsData)
+        setFilteredFriends(dumbFriendsData)
+        setFriendsCounter(dumbFriendsData.length)
+    }, [])
+
+    const onChangeQuery = (query) => setQuery(query)
+
+    return (
+        <div className={`${styles.div}`}>
+            <div>
+                <h6>Friends <span>({friendsCounter})</span></h6>
+                <img src={AddIcon} alt="" />
+            </div>
+            <Searchbar query={query} onChange={onChangeQuery} />
+            <div className={`${styles.friend_list}`}>
+                {filteredFriends.map((friend) => {
+                    return (
+                        <div>
+                            <div className={`${styles.profile}`}>
+                                <img src={friend.picture} alt="" />
+                                <div>
+                                    <div>
+                                        <span>{friend.username}</span>
+                                        <span>{friend.begin_on}</span>
+                                    </div>
+                                    <span>Code: {friend.code}</span>
+                                </div>
+                            </div>
+                            <div className={`${styles.actions}`}>
+                                <img src={DeleteIcon} alt="" />
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Friends
