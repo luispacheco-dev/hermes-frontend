81a0fc1171318657f43c5ab8199e6b7544a56ebe
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-08 17:33:00 -0700

    Add friend component

---
+++ src/components/friends/AddFriend.jsx
@@ -0,0 +1,28 @@
+import styles from "./AddFriend.module.css"
+import CloseIcon from "../../assets/add.svg"
+
+/*
+ * Add friend component 
+ *
+ * @component
+ * @returns {JSX.Element} The rendered Add friend component
+ */
+
+function AddFriend({ onClick }) {
+    return (
+        <div className={`${styles.div}`}>
+            <form className={`${styles.form}`}>
+                <div>
+                    <h6>Add Friend</h6>
+                    <img src={CloseIcon} alt="" onClick={onClick} />
+                </div>
+                <div>
+                    <input type="text" placeholder="Code" />
+                    <button type="submit">Add</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default AddFriend
