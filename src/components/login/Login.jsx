3c0081595cb81336e09830d4257510d216f9f459
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-12 10:58:25 -0700

    Login call added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,6 @@
 import styles from "./Login.module.css"
+import { useNavigate } from "react-router-dom"
+import { login } from "../../lib/services/auth"
 
 /*
  * Login page
@@ -8,9 +10,22 @@ import styles from "./Login.module.css"
  */
 
 function Login() {
+    const navigate = useNavigate()
+
+    const handleSumbit = (event) => {
+        event.preventDefault()
+        const payload = {
+            profile_id: "1",
+            access: "access",
+            refresh: "refresh",
+        }
+        login(payload)
+        navigate("/")
+    }
+
     return (
         <div className={`${styles.div}`}>
-            <form className={`${styles.form}`}>
+            <form className={`${styles.form}`} onSubmit={(e) => handleSumbit(e)}>
                 <h6>Hermes</h6>
                 <span>Don't have an account? <a href="/register">Create an account</a></span>
                 <input type="email" placeholder="Email" name="email" />
