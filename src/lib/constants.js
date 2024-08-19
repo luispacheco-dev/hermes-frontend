965a7d016bd47251fca68760eae393d8417852b0
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 16:55:55 -0700

    Profile Base Url Endpoint

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,4 +2,5 @@ const PORT = ":8000"
 const HOST = "http://127.0.0.1"
 
 export const BASE_URL = `${HOST}${PORT}`
-export const AUTH_ENDPOINT = `${BASE_URL}/auth`
+export const AUTH_BASE = `${BASE_URL}/auth`
+export const PROFILE_BASE = `${BASE_URL}/api/profiles/`
