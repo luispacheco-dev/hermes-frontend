279a4240989babf71b31e9df72757f270d802c9a
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 17:14:29 -0700

    Remove Slash

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,4 +3,4 @@ const HOST = "http://127.0.0.1"
 
 export const BASE_URL = `${HOST}${PORT}`
 export const AUTH_BASE = `${BASE_URL}/auth`
-export const PROFILE_BASE = `${BASE_URL}/api/profiles/`
+export const PROFILE_BASE = `${BASE_URL}/api/profiles`
