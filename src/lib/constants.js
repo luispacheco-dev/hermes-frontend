f6988ae55d446a673880616177ce84dd2ae084a9
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-20 13:06:06 -0700

    Friend Endpoint Base Url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,3 +4,4 @@ const HOST = "http://127.0.0.1"
 export const BASE_URL = `${HOST}${PORT}`
 export const AUTH_BASE = `${BASE_URL}/auth`
 export const PROFILE_BASE = `${BASE_URL}/api/profiles`
+export const FRIEND_BASE = `${BASE_URL}/api/friends`
