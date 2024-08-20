13e2f0931e9e680cb7e5cb81ff5fe070f041d3c0
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-20 12:00:21 -0700

    Update Profile Call

--- src/lib/services/profile.js
+++ src/lib/services/profile.js
@@ -86,3 +86,33 @@ export async function getProfile() {
     const data = await response.json()
     return { success: true, data: data }
 }
+
+export async function updateProfile(formData) {
+    const id = getProfileId()
+    const url = `${PROFILE_BASE}/${id}/`
+
+    const accessToken = await getAccess()
+    const authorization = `Bearer ${accessToken}`
+
+    const response = await fetch(url, {
+        method: "PATCH",
+        body: formData,
+        headers: {
+            Authorization: authorization
+        }
+    }).catch((error) => error)
+
+    if (response.status === 500) {
+        return { success: false, error: "Internal Error" }
+    }
+    if (response.status === 400) {
+        return { success: false, error: "Profile Doesn't Exist" }
+    }
+    if (response.status !== 200) {
+        console.log(await response.json())
+        return { success: false, error: "Something goes wrong" }
+    }
+
+    const data = await response.json()
+    return { success: true, data: data }
+}
