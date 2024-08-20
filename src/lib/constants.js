f9d9cb445028f5187db1aa1b03997af757e30830
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-20 15:59:55 -0700

    Chat Base Url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,3 +5,4 @@ export const BASE_URL = `${HOST}${PORT}`
 export const AUTH_BASE = `${BASE_URL}/auth`
 export const PROFILE_BASE = `${BASE_URL}/api/profiles`
 export const FRIEND_BASE = `${BASE_URL}/api/friends`
+export const CHAT_BASE = `${BASE_URL}/api/chats`
