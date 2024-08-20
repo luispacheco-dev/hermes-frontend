fc101d8cad3f9620f6d46b62845831aee9104045
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-19 17:15:36 -0700

    Update Profile Logged State Call

--- src/lib/services/profile.js
+++ src/lib/services/profile.js
@@ -1,4 +1,5 @@
 import { PROFILE_BASE } from "../constants"
+import { getAccess, getProfileId } from "../stores/session"
 
 /*
  * register
@@ -8,7 +9,7 @@ import { PROFILE_BASE } from "../constants"
  */
 
 export async function register(payload) {
-    const url = `${PROFILE_BASE}`
+    const url = `${PROFILE_BASE}/`
 
     const response = await fetch(url, {
         method: "POST",
@@ -28,4 +29,34 @@ export async function register(payload) {
     }
 
     return { success: true, data: "Account created" }
+}
+
+export async function updateLogged(logged) {
+    const id = getProfileId()
+    const url = `${PROFILE_BASE}/${id}/logged/`
+
+    const accessToken = getAccess()
+    const authorization = `Bearer ${accessToken}`
+
+    const response = await fetch(url, {
+        method: "PATCH",
+        body: JSON.stringify({ logged: logged }),
+        headers: {
+            Authorization: authorization,
+            "Content-Type": "application/json"
+        }
+    }).catch((error) => error)
+
+    if (response.status === 500) {
+        return { success: false, error: "Internal Error" }
+    }
+    if (response.status === 400) {
+        return { success: false, error: "Logged Field Is Required" }
+    }
+    if (response.status !== 200) {
+        console.log(await response.json())
+        return { success: false, error: "Something goes wrong" }
+    }
+
+    return { success: true, data: "" }
 }
