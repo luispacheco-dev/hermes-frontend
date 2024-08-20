c1b46d28891a885bfa287caed12ffb6e075603f6
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-20 13:05:51 -0700

    Send Friend Request Call

---
+++ src/lib/services/friend.js
@@ -0,0 +1,37 @@
+import { FRIEND_BASE } from "../constants";
+import { getAccess } from "../stores/session.js"
+import { getProfileId } from "../stores/session.js"
+
+export async function sendFriendRequest(code, greetings) {
+    const id = getProfileId()
+    const url = `${FRIEND_BASE}/friend-requests/`
+
+    const accessToken = await getAccess()
+    const authorization = `Bearer ${accessToken}`
+
+    const payload = {
+        sender: id,
+        code: code,
+        greetings: greetings ? greetings : "Hellop"
+    }
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+            Authorization: authorization,
+            "Content-Type": "application/json"
+        }
+    }).catch((error) => error)
+
+    if (response.status === 500) {
+        return { success: false, error: "Internal Error" }
+    }
+    if (response.status === 400) {
+        return { success: false, error: "Already Sended or Code Doesn't Exist" }
+    }
+    if (response.status !== 201) {
+        return { success: false, error: "Something Goes Wrong" }
+    }
+
+    return { success: true, data: "" }
+}
