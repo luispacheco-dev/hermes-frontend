2640dd9d20e4f5de3c9b5bde014a2001f1764098
Luis Pacheco <pachecoluis.dev@gmail>
2024-08-20 13:26:27 -0700

    Parse Timestamp

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,3 +3,8 @@ import { BASE_URL } from "./constants.js"
 export function getPictureUrl(url) {
     return `${BASE_URL}/${url}`
 }
+
+export function parseDateTime(timestamp) {
+    let time = timestamp.split("T")[1]
+    return time.split(".")[0]
+}
